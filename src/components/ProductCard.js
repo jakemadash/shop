cd83da86493aaf17648128d437b2befb455e7e0f
Jake Ashton <jakemaddenashton@gmail.com>
2023-01-25 20:23:29 -0700

    Finish styling product cards

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -10,27 +10,25 @@ const ProductCard = ({ item, updateCart }) => {
     minimumFractionDigits: 2,
   });
 
- const handleChange = (e) => {
-  setQuantity(parseInt(e.target.value))
- }
+  const handleChange = (e) => {
+    setQuantity(parseInt(e.target.value));
+  };
 
- const handleClick = () => {
-  updateCart(item.title, item.price, item.image, quantity)
- }
+  const handleClick = () => {
+    updateCart(item.title, item.price, item.image, quantity);
+  };
 
   return (
     <div className="product-card">
       <img src={item.image} alt={item.title}></img>
-      <div className="details">
-        <div>{item.title}</div>
-        <div>{usd.format(item.price)}</div>
-        <input
-          type="number"
-          defaultValue={1}
-          min={1}
-          onChange={handleChange}
-        ></input>
-      </div>
+      <div className="title">{item.title}</div>
+      <div className="price">{usd.format(item.price)}</div>
+      <input
+        type="number"
+        defaultValue={1}
+        min={1}
+        onChange={handleChange}
+      ></input>
       <button onClick={handleClick}>Add to cart</button>
     </div>
   );
