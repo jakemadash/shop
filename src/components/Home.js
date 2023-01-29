b067b6a97de45e24b67377c9051ce1f17a9ebd43
Jake Ashton <jakemaddenashton@gmail.com>
2023-01-28 20:21:41 -0700

    Finish media query styling for home page, start for product page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <div className="home">
         <div className="logo" id="big">Pop Shop</div>
         <div className="home-body">
