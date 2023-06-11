e5a226cc66867b60f599be5a3d57cbbe9181b394
Jake Ashton <jakemaddenashton@gmail.com>
2023-06-11 11:15:10 -0700

    Fix navigation error when clicking logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,12 +10,16 @@ const Header = ({ cartCount }) => {
   return (
     <div className="header-container">
       <header className="header nav">
-        <a href="/" className="logo no-transition">
+        <a href="#/" className="logo no-transition">
           Pop Shop
         </a>
         <nav className="main-menu">
-          <a href="#/" className="menu-item">Home</a>
-          <a href="#/products" className="menu-item">Products</a>
+          <a href="#/" className="menu-item">
+            Home
+          </a>
+          <a href="#/products" className="menu-item">
+            Products
+          </a>
           <CartIcon count={cartCount} />
         </nav>
       </header>
